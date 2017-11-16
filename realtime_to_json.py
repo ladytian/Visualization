f767ad68369b6db8cec50d51720af1249d8c4ed2
lady <2511240622@qq.com>
2017-11-16 17:06:35 +0800

    Add some tips
    1. Add the field "status"
    2. I have three methods to realize  getting data demanded by Yiyi, they are methods 1、2、3 and I have marked in the code.
       methods 1: return a file's content
       methods 2: set a fixed file
       methods 3: return a list that is a string.

--- realtime_to_json.py
+++ realtime_to_json.py
@@ -15,24 +15,25 @@ def room(ap):
     else:
         return ""
     
-    
 def main():
 
-    for root, dirs, files in os.walk('./5csv_realtime'):
+    for root, dirs, files in os.walk('./5csv_realtime/csv_realtime'):
         
         for filename in files:
+
+            #lt = []  # methods 3: return a list or you can say a string.
             
             reader = csv.reader(open(root + '/' + filename))
 
-            output = open('D:/workspace/Visualization/5json_realtime/' + filename + '.json', 'a')
+            output = open('D:/workspace/Visualization/5json_realtime/' + filename[0:-4] + '.json', 'a')
+            #output = open('D:/workspace/Visualization/5json_realtime' + "visualization" + '.json', 'w') # methods 2: only a fixed file.
             
             output.write('[')
 
             data_dict = {}
 
             reader.next()
-            for AP_Name,associationTime,clientMacAdd in reader:
-
+            for AP_Name, associationTime, clientMacAdd, status in reader:
 
                 if len(AP_Name) < 1:
                     continue
@@ -41,14 +42,31 @@ def main():
                 data_dict['associationTime'] = associationTime
                 data_dict['building'] = building(AP_Name)
                 data_dict['room'] = room(AP_Name)
+                data_dict['status'] = status
 
                 json.dump(data_dict, output)
                 output.write(',')
+
+                #lt.append(data_dict) #methods 3
 
             json.dump(data_dict, output)
             output.write(']')
             output.close()
 
+            #return lt  #methods 3
+
+# methods 1: return a file's content.
+def to_web_file():
+    for root, dirs, files in os.walk('./5json_realtime'):
+        
+        for filename in files:
+            file = open(root + '/' + filename)
+            return file
 
 if __name__ == '__main__':
     main()
+    #s = main() # methods 3
+    #print s[0:10] # methods 3
+    #sjson = to_web_file()  # methods 1
+    #print type(sjson), sjson  # methods 1
+
